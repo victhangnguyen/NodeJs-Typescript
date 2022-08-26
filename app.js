7afdf1f7b9fa67d9bc00c60c1c3a938f2aed5b6e
victhangnguyen <victhangnguyen@outlook.com>
2022-08-27 02:49:46 +0700

    190. Executing TypeScript Code with Node.js

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log('Something');
+//! node app.ts => Run => So is Node capable of executing TypeScript
+//! NO => It'sure
+//! It's done, because it just doesn't care about he file extension
+var age;
+age = 30;
+console.log(age);
+//! node app.ts => Run => get Error
+//! using tsc to compile TypeScript to JavaScript to node can run
---
+++ app.ts
@@ -0,0 +1,12 @@
+console.log('Something');
+//! node app.ts => Run => So is Node capable of executing TypeScript
+//! NO => It'sure
+//! It's done, because it just doesn't care about he file extension
+
+let age: number;
+age = 30;
+console.log(age);
+//! node app.ts => Run => get Error
+
+//! using tsc to compile TypeScript to JavaScript to node can run
+
